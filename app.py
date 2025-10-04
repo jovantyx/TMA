741652927b65c40e12e9b3244dfb6b4dbcfc8de3
jovantyx <tyxjovan@gmail.com>
2025-10-04 21:45:30 +0800

    Made details function worked

--- app.py
+++ app.py
@@ -30,3 +30,23 @@ def show_base():
         panel='BOOK TITLES',
         selected_category=selected_category # Used to keep the dropdown selected
     )
+
+@app.route('/bookdetails', methods=['GET'])
+def show_details():
+    # 1. Get the book title from the URL query parameter
+    book_title = request.args.get('title')
+    
+    # 2. Search for the corresponding book in the all_books list
+    selected_book = next((book for book in all_books if book['title'] == book_title), None)
+    
+    # 3. Handle case where book is not found
+    if selected_book is None:
+        # You can handle this more gracefully, but for now, redirect back to the book list
+        return redirect(url_for('show_base'))
+
+    # 4. Pass the single book object to the new template
+    return render_template(
+        'books_details.html',
+        book=selected_book,
+        panel='BOOK DETAILS'
+    )
